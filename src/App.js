5673f76b62601436be994bd7d1633a7e56413745
Mehedi4467 <mehedi446768@gmail.com>
2022-04-30 23:27:18 +0600

    firebase hook install

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Blogs from "./Components/Blogs/Blogs";
 import Home from "./Components/Home/Home";
 import NotFound from "./Components/NotFound/NotFound";
 import Header from "./shared/Header/Header";
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
--- src/shared/CustomLink/CustomLink.js
+++ src/shared/CustomLink/CustomLink.js
@@ -1,21 +1,24 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function CustomLink({ children, to, ...props }) {
-    let resolved = useResolvedPath(to);
-    let match = useMatch({ path: resolved.pathname, end: true });
-  
-    return (
-      <div>
-        <Link
-          style={{ textDecoration: match ? "underline" : "none" }}
-          to={to}
-          {...props}
-        >
-          {children}
-        </Link>
-      
-      </div>
-    );
-  }
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
 
-  export default CustomLink;
+  return (
+    <div>
+      <Link
+        className={`block py-2 pr-4 pl-3 text-block  rounded md:bg-transparent  md:p-0 dark:text-white ${
+          match
+            ? "bg-blue-700 text-white underline underline-offset-8 md:text-blue-700"
+            : "bg-white md:text-black"
+        }`}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+    </div>
+  );
+}
+
+export default CustomLink;
