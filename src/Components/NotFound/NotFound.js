6343a7b82d5a14d7958fce97e593d0c6ddc5e902
Mehedi4467 <mehedi446768@gmail.com>
2022-04-30 17:11:24 +0600

    tailwind and notfound page created

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import notFound from '../../Images/pageNotFund.png';
+
 const NotFound = () => {
     return (
-        <div>
-            <h1>This is not found page </h1>
+        <div className='container mx-auto flex items-center justify-center mt-10'>
+            <img src={notFound}  alt="Not Found Images"></img>
         </div>
     );
 };
